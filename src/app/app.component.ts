fdf59f0078d0791be45203f3c54133c5a673e77a
Nishko <rowanander7@gmail.com>
2023-09-11 09:36:23 +1000

    fixed login and logout button to display correct item when user is either logged in or not

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from './auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
 
   isLoggedIn: boolean = false;
+  private authSubscription: Subscription;
 
   constructor(private authService: AuthService) {
     console.log('AppComponent initialized.');
+    this.authSubscription = new Subscription();
   }
 
   ngOnInit(): void {
-    this.isLoggedIn = this.authService.isAuthenticated(); // Use a method to check if the user is logged in
+    this.authSubscription = this.authService.getAuthStatus().subscribe(isAuthenticated => {
+      this.isLoggedIn = isAuthenticated;
+    });
   }
 
   onLogoutClick(): void {
     this.authService.logout();
-    this.isLoggedIn = false; // Update the login status after logout
+    // isLoggedIn will be updated through the subscription
+  }
+
+  ngOnDestroy(): void {
+    this.authSubscription.unsubscribe(); // Unsubscribe to prevent memory leaks
   }
 
   get loggedInUsername(): string {
-    const username = this.authService.getLoggedInUsername();
-    console.log('Username in AppComponent:', username);
-    return username;
+    return this.authService.getLoggedInUsername();
   }
 }
+
 
 
 
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { Router } from '@angular/router';
 
@@ -11,36 +11,36 @@ export class AuthService {
   private apiUrl = 'http://localhost:3000';
   private loggedInUsername: string = '';
   private loggedInEmail: string = '';
+  private authStatus = new BehaviorSubject<boolean>(this.isAuthenticated());
   roles: string[] = [];
   userId: number = 0;
 
   constructor(private http: HttpClient, private router: Router) { }
+
+  // Get Auth Status
+  getAuthStatus(): Observable<boolean> {
+    return this.authStatus.asObservable();
+  }
 
   // Logout Method
   logout(): void {
     this.loggedInUsername = '';
     this.roles = [];
     this.userId = 0;
-
-    // Navigate to the login page
+    this.authStatus.next(false);
     this.router.navigate(['/login']);
   }
 
   // Login Method
   login(email: string, password: string): Observable<any> {
-    console.log(`Sending email: ${email}, password: ${password}`);  // Debug log
     return this.http.post(`${this.apiUrl}/login`, { email, password }).pipe(
       tap((response: any) => {
         if (response && response.username) {
           this.loggedInUsername = response.username;
           this.loggedInEmail = response.email;
           this.roles = response.roles || [];
-
           this.userId = response.id || 0;
-
-          console.log('Response from /login:', response);
-          console.log('Setting loggedInUsername:', this.loggedInUsername);
-          console.log('Setting roles:', this.roles); // Debug roles
+          this.authStatus.next(true);
         }
       })
     );
@@ -59,7 +59,6 @@ export class AuthService {
   }
 
   getRoles(): string[] {
-    console.log('Returning roles:', this.roles);
     return this.roles;
   }
 
